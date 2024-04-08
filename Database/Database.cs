7854a5a4ea5646215896e779ece95a778db19c0d
Gabriel Squissato <biel.squissato@hotmail.com>
2024-04-07 23:12:31 -0300

    Adicionado a validação dos campos para as colunas do banco

--- Database/Database.cs
+++ Database/Database.cs
@@ -11,9 +11,12 @@ namespace ApiMyArquivos.Database
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Arquivo>().Property(p => p.Titulo).HasMaxLength(100);
+            modelBuilder.Entity<Arquivo>().HasIndex(p => p.Titulo).IsUnique(true);
+            modelBuilder.Entity<Arquivo>().Property(p => p.Descricao).HasMaxLength(2000);
+
             base.OnModelCreating(modelBuilder);
         }
-
     }
 
 }
